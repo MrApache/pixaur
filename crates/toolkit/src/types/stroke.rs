ef90daa575c32296027d9d80e567067b6f6c98ea
Irisu <mrapacheee@gmail.com>
2025-09-15 14:04:02 +0400

    refactor: add stroke color order comment

--- crates/toolkit/src/types/stroke.rs
+++ crates/toolkit/src/types/stroke.rs
@@ -2,6 +2,7 @@ use super::{Argb8888, Corners};
 
 #[derive(Clone)]
 pub struct Stroke {
+    ///Left, Right, Top, Bottom
     pub color: [Argb8888; 4],
     pub width: f32,
     pub corners: Corners,
